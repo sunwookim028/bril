17947494d79d36bb311c6a57c7974922827081d7
Sunwoo Kim <sunwookim028@dhcp-vl2041-4593.eduroam.cornell.edu>
2025-09-16 01:03:09 -0400

    added trivial dead code elimination

--- tasks/bb.py
+++ tasks/bb.py
@@ -5,46 +5,44 @@ import json
 import sys
 
 
+TERMINATORS = {'jmp', 'br', 'ret'}
+
+
 def form_blocks(func):
   """Returns a list of basic blocks from the function,
   where each block is a list of instructions.
   """
-  #FIXME!!
-  #unit test. return 1 block allways.
   cur_block = []
   for instr in func['instrs']:
+    if 'label' in instr:
+      if cur_block:
+        yield cur_block
+        cur_block = []
     cur_block.append(instr)
+    if 'label' not in instr and instr['op'] in TERMINATORS:
+      yield cur_block
+      cur_block = []
   if cur_block:
     yield cur_block
 
 
-if __name__ == '__main__':
+def print_blocks(func):
+  """Prints out the block to stdout.
+  """
   import briltxt
+  for i, block in enumerate(form_blocks(func)):
+    # print out block header
+    if 'label' in block[0]:
+      print(f"{func['name']}.block{i} ({block[0]['label']}):")
+      block = block[1:]
+    else:
+      print(f"{func['name']}.block{i}:")
+    for instr in block:
+      # indent then print the json instr in a txt form.
+      print("  {}".format(briltxt.instr_to_string(instr)))
+
+
+if __name__ == '__main__':
   bril = json.load(sys.stdin)
   for func in bril['functions']:
-    blocks = form_blocks(func)
-    for i, block in enumerate(blocks):
-      if 'label' in block[0]:
-        print(f"{func['name']}.block{i} ({block[0]['label']}):")
-        block = block[1:]
-      else:
-        print(f"{func['name']}.block{i}:")
-      for instr in block:
-        # indent then print the json instr in a txt form.
-        print("  {}".format(briltxt.instr_to_string(instr)))
-    '''
-		bb_id = 0
-		was_jb = False
-		first = True
-		for instr in func['instrs']:
-			if 'label' in instr or was_jb or first:
-				print(f"bb #{bb_id}")
-				bb_id = bb_id + 1
-				if first:
-					first = False
-			print(instr)
-			if 'label' not in instr and instr['op'] in {'jmp', 'br', 'ret'}:
-				was_jb = True
-			else:
-				was_jb = False
-    '''
+    print_blocks(func)
---
+++ tasks/dce.py
@@ -0,0 +1,57 @@
+""" Trivial dead code elimination.
+"""
+
+import json
+import sys
+from bb import TERMINATORS
+
+SIDE_EFFECT = {'print', 'call'}
+
+
+def eliminate(block):
+  """ Given a basic block, a list of Bril instructions,
+  eliminate trivially dead instructions, i.e. assignments
+  that are overwritten before referred.
+  """
+  # @@FIXME!!
+  #return block
+  to_delete = set()
+  yet_unused = {}
+  for i, instr in enumerate(block):
+    if 'label' in instr: continue
+    if instr['op'] in TERMINATORS: continue
+    if instr['op'] in SIDE_EFFECT: continue
+    if instr['op'] == 'nop':  
+      to_delete.add(i)
+      continue
+    # all instrs that come here has a dest variable.
+    # referenced instr gets alive 
+    if 'args' in instr:
+      for var in instr['args']:
+        if var in yet_unused:
+          del yet_unused[var]
+    dest = instr['dest']
+    # overwritten instr is dead
+    if dest in yet_unused:
+      to_delete.add(yet_unused[dest])
+    yet_unused[dest] = i
+  for i, instr in enumerate(block):
+    if i not in to_delete:
+      yield instr
+
+
+if __name__ == '__main__':
+  from bb import form_blocks
+  from briltxt import print_func
+  bril = json.load(sys.stdin)
+  for func in bril['functions']:
+    changed = True
+    while changed:
+      live_instrs = []
+      orig_instr_cnt = len(func['instrs'])
+      for block in form_blocks(func):
+        live_instrs += eliminate(block)
+        func['instrs'] = live_instrs
+      new_instr_cnt = len(func['instrs'])
+      changed = new_instr_cnt < orig_instr_cnt
+    print_func(func)
