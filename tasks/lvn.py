b12658672cb3294bb4769f8b6c206ee18fbdd537
Sunwoo Kim <sunwookim028@dhcp-vl2041-40125.eduroam.cornell.edu>
2025-09-16 23:23:11 -0400

    lvn works for non-dest ops

--- tasks/lvn.py
+++ tasks/lvn.py
@@ -14,11 +14,12 @@ def eliminate_common(block):
      basic block, where (some) common subexprs are 
      eliminated.
   """
+  new_block = []
   # Handle label and terminators.
   if 'label' in block[0]:
-    yield block[0]
+    new_block.append(block[0])
     block = block[1:]
-  term = ""
+  term = None
   if (block and block[-1] and
       block[-1]['op'] in TERMINATORS):
     term = block[-1]
@@ -26,37 +27,70 @@ def eliminate_common(block):
   # Key data structures.
   # prev_rhs and canonical_vars share indices, 
   # which are values of the lvn mapping.
-  var_to_lvn = {}
+  var_to_lvn = dict()
   prev_rhs = [] # if binary op, always lvn1<lvn2.
   canonical_vars = []
+  # MAYBE TODO add read_first vars.
+  # assign Nones for prev_rhs and canon.. entries.
+  # CURRENTLY we assign the next i at discoveries.
+  read_first_lvn = -1
   # Main loop
   for instr in block:
     if 'dest' not in instr:
-      yield instr
+      new_block.append(instr)
+      continue
     # Core logic.
     # 1/2, Find match in the canonical tuple form.
-    hit = False
-    for i, some_prev_rhs in enumerate(prev_rhs):
-      # of the rhs of this instruction, which is
-      # a constant or a value operation.
-      if instr['op'] == 'const':
-        rhs_in_lvn = () #FIXME
+    if instr['op'] == 'const':
+      rhs_in_lvn = ('const', instr['value'])
+    else:
+      args_in_lvn = []
+      for arg in instr['args']:
+        # handle args from other blocks above
+        if arg not in var_to_lvn.keys():
+          next_lvn = len(prev_rhs)
+          var_to_lvn[arg] = next_lvn
+          prev_rhs.append(None)
+          canonical_vars.append(None)
+        args_in_lvn.append(var_to_lvn[arg])
+      args_in_lvn.sort()
+      if 'funcs' in instr:
+        rhs_in_lvn = ['call', instr['func']]
       else:
-        rhs_in_lvn = () #FIXME
+        rhs_in_lvn = [instr['op']]
+      if args_in_lvn:
+        rhs_in_lvn = rhs_in_lvn + args_in_lvn
+      rhs_in_lvn = tuple(rhs_in_lvn)
+    hit = False
+    i = 0
+    for some_prev_rhs in prev_rhs:
       if rhs_in_lvn == some_prev_rhs:
         hit = True
         break
-    # 2/2, yield instr in the correct form
-    # and update the 3 data structures.
-    if hit: # Matched
-      # TODO. update the 3 data structures
-      # index is i.
-      yield instr # FIXME. yield transformed instr
+      i = i + 1
+    # 2/2, update the 3 data structures
+    # and append instr in the correct form.
+    var_to_lvn[instr['dest']] = i
+    if hit:
+      # prev_rhs and canonical_vars don't update.
+      instr["op"] = "id"
+      instr["args"] = [canonical_vars[i]]
     else:
-      # TODO. update the 3 data structures
-      yield instr
+      prev_rhs.append(rhs_in_lvn)
+      canonical_vars.append(instr["dest"])
+      if "args" in instr:
+        new_args = []
+        for arg in instr["args"]:
+          lvn = var_to_lvn[arg]
+          if canonical_vars[lvn] != None:
+            new_args.append(canonical_vars[lvn])
+          else:
+            new_args.append(arg)
+        instr["args"] = new_args
+    new_block.append(instr)
   if term:
-    yield term
+    new_block.append(term)
+  return new_block
 
 
 def lvn_pass(func):
@@ -67,9 +101,7 @@ def lvn_pass(func):
   from bb import form_blocks
   new_instrs = []
   for block in form_blocks(func):
-    new_block = eliminate_common(block)
-    for instr in new_block:
-      new_instrs.append(instr)
+    new_instrs = new_instrs + eliminate_common(block)
   func['instrs'] = new_instrs
   return func
     
